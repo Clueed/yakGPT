f9c060646adefa009affcd40371ae29ae392b772
Clueed <clueed@proton.me>
2023-04-27 13:00:49 +0000

    wip: styling

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE_BUNDLE === 'true',
-})
-
-
-module.exports = withBundleAnalyzer({
+const defaultConfig = {
   reactStrictMode: false,
-})
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
+if (process.env.ANALYZE === "true") {
+  const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: true,
+  });
 
+  module.exports = withBundleAnalyzer(defaultConfig);
+} else {
+  module.exports = defaultConfig;
+}
